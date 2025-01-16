56d0157295717b71c4138d65ff0301aec9a80d48
Jason Rodrigues <jasonrodrigues28@gmail.com>
2025-01-16 10:08:28 +0530

    Modified the code to take user input

--- main.py
+++ main.py
@@ -34,7 +34,7 @@ def download_files(base_url, save_dir):
     print(f"{file_count} files downloaded.")
 
 # Example Usage
-base_url = "http://192.168.1.10/LD/Cloud/EBOOKS/"  # Change to your URL
-save_dir = r"C:\Users\Jason\Desktop\IntraSave"  # Change to your save folder
+base_url = input("PASTE THE URL HERE: ")
+save_dir = r"C:\Users\Jason\Desktop\IntraSave\MCN\UNIT 4"  # Change to your save folder
 download_files(base_url, save_dir)
 print("Download finished!")
